af94adce1e97cbd7ec80b103e5fc51d016d1df5c
PatrickCoakley23 <patrickcoakley23@gmail.com>
2020-11-30 17:12:01 +0000

    added dropdown menu for filtering properties by price low or high

--- properties/views.py
+++ properties/views.py
@@ -9,10 +9,27 @@ def properties(request):
 
     properties = Property.objects.all()
     propFilter = PropertyFilter(request.GET, queryset=properties)
+    sort = None 
+    direction = None 
+
+    if request.GET: 
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                properties = Property.annotate(lower_name=Lower('name'))
+            
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            properties = properties.order_by(sortkey)
+            propFilter = PropertyFilter(request.GET, queryset=properties)
+
 
     context = {
     'filter': propFilter
 }
   
-
     return render(request, 'properties/properties.html', context )
