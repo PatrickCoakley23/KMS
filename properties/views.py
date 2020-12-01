b185e6c9338c91d5b3bb60f575d2f152ddbaedd9
PatrickCoakley23 <patrickcoakley23@gmail.com>
2020-12-01 18:32:07 +0000

    implemented pagition but have issues with it working alongside django-filter

--- properties/views.py
+++ properties/views.py
@@ -1,16 +1,23 @@
 from django.shortcuts import render
 from django.db.models import Max
+from django.core.paginator import Paginator
 from .models import Property, Type, County
 from .filters import PropertyFilter
 
 
 def properties(request):
     """ A view to show all products, including sorting and search queries """
-
+    context = {}
     properties = Property.objects.all()
     propFilter = PropertyFilter(request.GET, queryset=properties)
     sort = None 
     direction = None 
+
+    context['filter'] = propFilter
+
+    paginated_properties = Paginator(propFilter.qs, 2)
+    page_number = request.GET.get('page')
+    page_obj = paginated_properties.get_page(page_number)
 
     if request.GET: 
         if 'sort' in request.GET:
@@ -26,10 +33,12 @@ def properties(request):
                     sortkey = f'-{sortkey}'
             properties = properties.order_by(sortkey)
             propFilter = PropertyFilter(request.GET, queryset=properties)
+            
 
 
     context = {
-    'filter': propFilter
+    'filter': propFilter, 
+    'page_obj': page_obj
 }
   
     return render(request, 'properties/properties.html', context )
