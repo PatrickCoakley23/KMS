7184cb2b19c0fc87e627890a41b062e3dbfa0c1e
PatrickCoakley23 <patrickcoakley23@gmail.com>
2020-12-02 11:14:27 +0000

    pagination working but must now align it with sort functionality

--- properties/templatetags/properties_extras.py
+++ properties/templatetags/properties_extras.py
@@ -3,9 +3,8 @@ from django import template
 register = template.Library()
 
 @register.simple_tag
-def my_url(value, field_name, urlencode=None):
-    url = '/?{}={}'.format(field_name, value)
-    
+def properties_url(value, field_name, urlencode=None):
+    url = '?{}={}'.format(field_name, value)
     if urlencode:
         querystring = urlencode.split('&')
         filtered_querystring = filter(lambda p: p.split('=')[0] != field_name, querystring)
