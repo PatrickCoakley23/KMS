eb714617f32768820e079c6783b560a6d222541d
PatrickCoakley23 <patrickcoakley23@gmail.com>
2021-01-14 17:32:00 +0000

    updated testimonial section from static html to iterating from python model

--- home/models.py
+++ home/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+class Testimonial(models.Model):
+    clients_name = models.CharField(max_length=254)
+    comment = models.TextField(max_length=350)
+    date_published = models.DateField(null=False)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.clients_name
--- home/views.py
+++ home/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
+from .models import Testimonial
 
 # Create your views here.
 
 def index(request):
     """ a view to return the index page """
+    testimonials = Testimonial.objects.all()
 
-    return render(request, 'home/index.html')
+    context = {
+        'testimonials': testimonials
+    }
+
+    return render(request, 'home/index.html', context)
