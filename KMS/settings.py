0ee08c59e4cede46d5a60020e242cb80a7371950
PatrickCoakley23 <patrickcoakley23@gmail.com>
2021-01-15 12:26:52 +0000

    set debug to true in development

--- KMS/settings.py
+++ KMS/settings.py
@@ -25,8 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('KMS_SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
+DEBUG = 'DEVELOPMENT' in os.environ
 ALLOWED_HOSTS = ['keane-mahony-smith.herokuapp.com', 'localhost']
 
 
