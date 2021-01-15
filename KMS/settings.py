90eae6fe43cb2aeb4076b2ad7df0f1fdfc3451e2
PatrickCoakley23 <patrickcoakley23@gmail.com>
2021-01-15 11:19:58 +0000

    trying to solve h10 error with heroku

--- KMS/settings.py
+++ KMS/settings.py
@@ -103,9 +103,9 @@ if 'DATABASE_URL' in os.environ:
     }
 else:
     DATABASES = {
-    'default': {
+        'default': {
             'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR / 'db.sqlite3'),
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
 
--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn KMS.wsgi:application 
+web: gunicorn KMS.wsgi:application
