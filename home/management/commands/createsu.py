962d967d433ea521b97851a8b003fd6243dd80f5
PatrickCoakley23 <patrickcoakley23@gmail.com>
2021-03-09 15:51:02 +0000

    deleting passwords and username

--- home/management/commands/createsu.py
+++ home/management/commands/createsu.py
@@ -6,4 +6,4 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         if not User.objects.filter(username="KeaneMahonySmith").exists():
-            User.objects.create_superuser("patrickcoakley23", "patrickcoakley23@gmail.com", "Melonhead23")
+            User.objects.create_superuser("", "", "")
