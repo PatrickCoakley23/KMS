89d757f3601ff69d0fe8b0a5c4e5807419e61644
PatrickCoakley23 <patrickcoakley23@gmail.com>
2021-03-09 11:06:25 +0000

    added ebextensions python

--- KMS/settings.py
+++ KMS/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('KMS_SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
-ALLOWED_HOSTS = ['keane-mahony-smith.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = [#'keane-mahony-smith.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -100,7 +100,7 @@ WSGI_APPLICATION = 'KMS.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
+"""
 if 'DATABASE_URL' in os.environ:
     DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
@@ -112,7 +112,7 @@ else:
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
-
+"""
 #DATABASES = {
 #   'default': dj_database_url.parse()
 # }
