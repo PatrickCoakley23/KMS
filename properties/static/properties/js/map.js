ade7291ffb963d3726e49ca370741f3bc40d28d0
PatrickCoakley23 <patrickcoakley23@gmail.com>
2021-03-10 09:54:58 +0000

    made content changes for darragh and formatted css + js

--- properties/static/properties/js/map.js
+++ properties/static/properties/js/map.js
@@ -1,35 +1,35 @@
- var latitude = $('#id_latitude').text().slice(1, -1);
-        var lat = parseFloat(latitude);
-        var longitude = $('#id_longitude').text().slice(1, -1);
-        var lng = parseFloat(longitude);
+var latitude = $('#id_latitude').text().slice(1, -1);
+var lat = parseFloat(latitude);
+var longitude = $('#id_longitude').text().slice(1, -1);
+var lng = parseFloat(longitude);
 
-        function initMap() {
+function initMap() {
 
-            var map = new google.maps.Map(document.getElementById("map"), {
-                zoom: 13,
-                center: {
-                    lat: lat,
-                    lng: lng
-                }
-            });
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 13,
+        center: {
+            lat: lat,
+            lng: lng
+        }
+    });
 
-            var labels = "ABCDEFGHIJKLMONPQRSTUVWXYZ";
+    var labels = "ABCDEFGHIJKLMONPQRSTUVWXYZ";
 
-            var locations = [{    
-                lat: lat,
-                lng: lng
-            }];
+    var locations = [{
+        lat: lat,
+        lng: lng
+    }];
 
-            var markers = locations.map(function(location, i) {
-                return new google.maps.Marker({
-                    position: location,
-                    label: labels[i % labels.length]
-                });
-            });
+    var markers = locations.map(function(location, i) {
+        return new google.maps.Marker({
+            position: location,
+            label: labels[i % labels.length]
+        });
+    });
 
-            var markerCluster = new MarkerClusterer(map, markers, {
-                imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'
-            });
+    var markerCluster = new MarkerClusterer(map, markers, {
+        imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'
+    });
 
 
-        } 
+}
