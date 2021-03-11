e9e4ffc13e8438ea326e2138d34a3acd833521d5
PatrickCoakley23 <patrickcoakley23@gmail.com>
2021-03-11 19:22:31 +0000

    added /kms url

--- home/urls.py
+++ home/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('/kms', views.index, name='home'),
     path('selling_guide/', views.SellingGuide, name='SellingGuide'),
     path('contact/', views.Contact, name='ContactUs'),
 ]
