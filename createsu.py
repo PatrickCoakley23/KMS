4d940fe9f02391dbb7f90c98fc0a2c9de9da1a54
patrick <patrickcoakley23@gmail.com>
2022-02-25 17:06:26 +0000

    super user

--- createsu.py
+++ createsu.py
@@ -3,5 +3,5 @@ from apps.authentication.models import User
 
 class Command(BaseCommand):
   def handle(self, *args, **options):
-      if not User.objects.filter(email="patrickcoakley23@gmail.com").exists(): #change email for username if you user django User model
-        User.objects.create_superuser( "patrickcoakley23@gmail.com", "admin", "020408") # email/username and the password
+      if not User.objects.filter(email="patrickcoakley23@outlook.com").exists(): #change email for username if you user django User model
+        User.objects.create_superuser( "patrickcoakley23@outlook.com", "020408") # email/username and the password
