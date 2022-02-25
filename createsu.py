5ce60f3cf5a07a76a204c7a5af701367d53fdab9
patrick <patrickcoakley23@gmail.com>
2022-02-25 17:31:23 +0000

    super user

--- createsu.py
+++
@@ -1,6 +0,0 @@
-from django.core.management.base import BaseCommand
-from apps.authentication.models import User
-
-class Command(BaseCommand):
-  def handle(self, *args, **options):
-        User.objects.create_superuser( "patrickcoakley23@outlook.com", "020408") # email/username and the password
