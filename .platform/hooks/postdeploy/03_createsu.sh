9f469a9a8e8f5443915a23ef450134da5376c4a7
patrick <patrickcoakley23@gmail.com>
2022-02-25 17:39:35 +0000

    super user

--- .platform/hooks/postdeploy/03_createsu.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-source /var/app/venv/*/bin/activate
-cd /var/app/staging
-from django.contrib.auth.models import User; User.objects.create_superuser( "patrick","patrickcoakley23@outlook.com", "020408")
