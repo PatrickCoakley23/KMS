eb54d83a3484dab4e65910481a98f9237441d60c
patrick <patrickcoakley23@gmail.com>
2025-10-28 17:10:04 +0000

    Updated allowed hosts

--- KMS/settings.py
+++ KMS/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('KMS_SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
-ALLOWED_HOSTS = ['KMS-AL2-Prod.us-west-2.elasticbeanstalk.com','KMS-Lin.eba-aucgvp2k.us-west-2.elasticbeanstalk.com','www.kmsdjango.eba-k3btpj5g.us-west-2.elasticbeanstalk.com','kmsdjango.eba-k3btpj5g.us-west-2.elasticbeanstalk.com', 'www.keanemahonysmith.ie',
+ALLOWED_HOSTS = ['KMS-AL2-Prod.us-west-2.elasticbeanstalk.com','kms2025-env.eba-h64jr7mx.us-west-2.elasticbeanstalk.com', 'www.keanemahonysmith.ie',
 'keanemahonysmith.ie', 'localhost',]
 
 
